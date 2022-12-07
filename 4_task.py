153ce6ffd2aae4dd26a1ca6200f1586c80820a6d
zelenskii <zelenskiy.ivan@gmail.com>
2022-12-07 23:00:17 +0300

    two memories

--- 4_task.py
+++ 4_task.py
@@ -4,3 +4,29 @@
 # Пример:
 # k=2 => 2*x^2 + 4*x + 5 или x^2 + 5 или 10*x**2
 # k=3 => 2*x^3 + 4*x^2 + 4*x + 5
+
+from random import randint
+
+coefficients = []
+k = int(input('Введите степень многочлена: '))
+for i in range(k + 1):
+    num = randint(- 10, 10)
+    coefficients.append(num)
+
+print(coefficients)
+result = ''
+for i in range(len(coefficients)):
+    if len(result) > 0 and coefficients[i] > 0:
+        result = result + ' + '
+    if coefficients[i] == 0:
+        continue
+
+    result = result + str(coefficients[i]) + 'X^' + str(k - i)
+
+if coefficients[- 1] != 0:
+    result = result[:len(result) - 3]
+result = result + ' = 0'
+print(result)
+
+with open('test.txt', 'w') as f:
+    f.write(result)
--- 5_task.py
+++ 5_task.py
@@ -6,3 +6,25 @@
 # 4*x**2 + 1*x + 4
 # Результирующий файл:
 # 6*x**2 + 5*x + 9
+
+import io
+from sympy import *
+
+NUM_STEPS = 2
+
+str_sum = ''
+list_source = []
+
+for i in range(1, NUM_STEPS + 1):
+    with io.open(f'file{i}.txt', 'r') as f:
+        list_source.append(f.readline()[:-1])
+
+str_sum = ((list_source[0] + list_source[1])[: - 2])
+print(str_sum)
+
+sum = simplify(str_sum)
+print(sum)
+
+f_out = io.open('file_sum.txt', 'w')
+print(sum, file=f_out)
+f_out.close()
